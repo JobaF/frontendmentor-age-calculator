cca49d0ab971c8c6e768fe2ab7c221607f436fe7
Jabo Fecht <jifecht@gmail.com>
2023-04-21 12:54:53 +0200

    Add google font and the inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css"
 
 function App() {
-	return <>Boiler</>
+	return (
+		<div className="font-poppins h-screen bg-gray-100 flex flex-col">
+			<div>
+				<label htmlFor="day">DAY</label>
+				<input type="text" id="day" name="day" />
+			</div>
+			<div>
+				<label htmlFor="month">MONTH</label>
+				<input type="text" id="month" name="month" />
+			</div>
+			<div>
+				<label htmlFor="year">YEAR</label>
+				<input type="text" id="year" name="year" />
+			</div>
+		</div>
+	)
 }
 
 export default App
